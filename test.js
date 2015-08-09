92a6b3a9959faa2926df23b8b103cbd842081d03
iCal <iCal@localhost.localdomain>
2015-08-09 20:50:51 +0700

    assertion testing checking result type

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ var assert = require('assert');
 var keydupl = require('./');
 
 it('should be in array object result', function () {
-	//
-	// assert.strictEqual(keydupl(['a']), { a: 'a' });
+	var test = keydupl(['a']);
+	assert(typeof test == 'object');
 });
